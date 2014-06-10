ca671138f525c9ec2b6733b3cf834587e14345a1
Sergey Chikuyonok <serge.che@gmail.com>
2014-06-10 18:44:44 +0400

    Added main preprocessor transformations

---
+++ index.js
@@ -0,0 +1,36 @@
+var DomUtils = require('domutils');
+var utils = require('./lib/utils');
+
+var transformers = [
+	require('./lib/transformer/root'),
+	require('./lib/transformer/if'),
+	require('./lib/transformer/attribute-element'),
+	require('./lib/transformer/attribute-set-element'),
+	require('./lib/transformer/attributes-expression'),
+	require('./lib/transformer/text')
+];
+
+function transform(node) {
+	for (var i = 0, il = transformers.length, res; i < il; i++) {
+		res = transformers[i](node);
+		if (res === false) {
+			// stop further transformations
+			return;
+		}
+	}
+
+	if (node.children) {
+		node.children.forEach(transform);
+	}
+}
+
+module.exports = {
+	transform: function(code) {
+		var dom = utils.makeDom(code);
+		dom.forEach(function(node) {
+			transform(node);
+		});
+
+		return DomUtils.getOuterHTML(dom[0]);
+	}
+};
--- sample.js
+++ sample.js
@@ -1,40 +1,8 @@
 var fs = require('fs');
-var htmlparser = require('htmlparser2');
-var DomHandler = require('domhandler');
-var DomUtils = require('domutils');
-var expression = require('expression-parser');
+var transformer = require('./index');
 
-var transformers = [
-	require('./lib/transformer/attribute')
-];
-
-function transform(node) {
-	transformers.forEach(function(t) {
-		t(node);
-	});
-
-	if (node.children) {
-		node.children.forEach(transform);
-	}
-}
-
-var handler = new DomHandler(function(err, dom) {
-	console.log('Transforming XML');
-	dom.forEach(function(node) {
-		if (node.name === 't:style') {
-			node.attribs['xmlns'] = 'http://www.w3.org/1999/xhtml';
-			node.attribs['xmlns:t'] = 'http://inn.ru/template';
-			node.attribs['xmlns:xsl'] = 'http://www.w3.org/1999/XSL/Transform';
-		}
-
-		transform(node);
-	});
-
-	var out = DomUtils.getOuterHTML(dom[0]);
-	console.log('Done!');
-	fs.writeFileSync('out.xml', out, {encoding: 'utf8'});
-});
-
-var parser = new htmlparser.Parser(handler, {xmlMode: true});
-parser.write(fs.readFileSync('example.xml', {encoding: 'utf8'}));
-parser.end();
+console.log('Transforming XML');
+var contents = fs.readFileSync('example.xml', {encoding: 'utf8'})
+var out = transformer.transform(contents);
+fs.writeFileSync('out.xml', out, {encoding: 'utf8'});
+console.log('Done!');
