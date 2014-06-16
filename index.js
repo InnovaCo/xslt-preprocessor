e5164d74ddbc1b51e864f4baf8ac2db19365f191
Sergey Chikuyonok <serge.che@gmail.com>
2014-06-16 19:44:51 +0400

    Added binary for preprocessing files

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var path = require('path');
+var fs = require('fs');
+var glob = require('glob');
+var chalk = require('chalk');
 var DomUtils = require('domutils');
 var utils = require('./lib/utils');
 
@@ -13,6 +17,11 @@ var transformers = [
 	require('./lib/transformer/xsl'),
 	require('./lib/transformer/text')
 ];
+
+function getCwd(options) {
+	var cwd = (options && options.cwd) || process.cwd();
+	return path.resolve(cwd);
+}
 
 function transform(node) {
 	for (var i = 0, il = transformers.length, res; i < il; i++) {
@@ -33,13 +42,66 @@ function transform(node) {
 	}
 }
 
-module.exports = {
-	transform: function(code) {
-		var dom = utils.makeDom(code);
-		dom.forEach(function(node) {
-			transform(node);
-		});
+function preprocessStylesheet(code) {
+	var dom = utils.makeDom(code);
+	dom.forEach(function(node) {
+		transform(node);
+	});
 
-		return DomUtils.getOuterHTML(dom[0]);
+	return DomUtils.getOuterHTML(dom[0]);
+}
+
+/**
+ * Processes given set of files
+ * @param  {Array}  files   Absolute path to files to prerocess
+ * @param  {Object} options Additional options
+ */
+function processFiles(files, options) {
+	var fsOpt = {encoding: 'utf8'};
+	files.forEach(function(f) {
+		if (!fs.existsSync(f)) {
+			return console.warn('File %s does not exists', chalk.bold.yellow(f));
+		}
+
+		fs.readFile(f, fsOpt, function(err, content) {
+			if (err) {
+				return console.error('Unable to open file %s: %s', chalk.red(f), err);
+			}
+			
+			console.log('Processing %s', chalk.green(f));
+			var out = preprocessStylesheet(content);
+			var targetFile = f;
+			if ('dest' in options) {
+				var relpath = path.relative(getCwd(options), f);
+				targetFile = path.resolve(path.join(options.dest, relpath));
+			}
+
+			fs.writeFile(targetFile, out, function(err) {
+				if (err) {
+					return console.log('Unable to write result to %s: %s', chalk.red(targetFile), err);
+				}
+				console.log('Writing result to %s', chalk.green(targetFile));
+			})
+		});
+	});
+}
+
+module.exports = {
+	transform: preprocessStylesheet,
+
+	/**
+	 * Transforms all files matched by given glob patter
+	 * @param  {Object} pattern Glob pattern, string or array of string
+	 * @param  {Object} options Options used for globbing and saving result
+	 */
+	preprocess: function(pattern, options) {
+		options = options || {};
+		glob(pattern, options, function(err, files) {
+			if (err) {
+				return console.error('Unable to find files for preprocessing: ' + err);
+			}
+
+			processFiles(files, options);
+		});
 	}
 };
